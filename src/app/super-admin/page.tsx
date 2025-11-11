822a35fd0a064dad2e439ade681576dcc1eccf36
Tradinta Commerce <tradintacommerce@gmail.com>
2025-11-11 12:05:58 +0000

    Try fixing this error: `Runtime Error: TypeError: Super expression must

--- src/app/super-admin/page.tsx
+++ src/app/super-admin/page.tsx
@@ -1,4 +1,6 @@
 
+'use client';
+
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
