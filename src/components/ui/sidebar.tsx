d74c23cfe061012cc507e01d703c92a82944ed93
Tradinta Commerce <tradintacommerce@gmail.com>
2025-11-11 12:26:19 +0000

    what is this space between sidebar and tab content when sidebar is expan

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -325,7 +325,7 @@ const SidebarInset = React.forwardRef<
       className={cn(
         "relative flex min-h-svh flex-1 flex-col bg-background",
         "md:peer-data-[state=expanded]:pl-[--sidebar-width]",
-        "md:peer-data-[state=collapsed]:peer-data-[collapsible=icon]:pl-[--sidebar-width-icon]]",
+        "md:peer-data-[state=collapsed]:peer-data-[collapsible=icon]:pl-[--sidebar-width-icon]",
         "transition-[padding] duration-200 ease-linear",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
